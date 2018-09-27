46715012dc90160d7b251ae025af35b7310828ad
lcj <thu512@naver.com>
2018-09-27 13:13:41 +0900

    Initial commit

---
+++ app/src/main/java/com/gsitm/bioauth/MainActivity.java
@@ -0,0 +1,163 @@
+package com.gsitm.bioauth;
+
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.biometrics.BiometricPrompt;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "지문";
+
+    private Button btn;
+    private BiometricPrompt mBiometricPrompt;
+
+    // Unique identifier of a key pair
+    private static final String KEY_NAME = "test";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+
+
+        btn.setOnClickListener((view) -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+
+                if (isSupportBiometricPrompt()) {
+                    Signature signature;
+                    try {
+                            signature = initSignature(KEY_NAME);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    // Create biometricPrompt
+                    mBiometricPrompt = new BiometricPrompt.Builder(this)
+                            .setDescription("Description")
+                            .setTitle("Title")
+                            .setSubtitle("Subtitle")
+                            .setNegativeButton("Cancel", getMainExecutor(), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Log.i(TAG, "Cancel button clicked");
+                                }
+                            })
+                            .build();
+                    CancellationSignal cancellationSignal = getCancellationSignal();
+                    BiometricPrompt.AuthenticationCallback authenticationCallback = getAuthenticationCallback();
+
+                    // Show biometric prompt
+                    if (signature != null) {
+                        Log.i(TAG, "Show biometric prompt");
+                        mBiometricPrompt.authenticate(new BiometricPrompt.CryptoObject(signature), cancellationSignal, getMainExecutor(), authenticationCallback);
+                    }
+                }
+
+            } else{ //FingerPrint사용
+                Log.i("지문", "API 27");
+
+
+            }
+        });
+
+    }
+
+
+    private boolean isSupportBiometricPrompt() {
+        PackageManager packageManager = this.getPackageManager();
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
+            return true;
+        }
+        return false;
+    }
+
+    //지문 인증 취소 이벤트
+    private CancellationSignal getCancellationSignal() {
+        // With this cancel signal, we can cancel biometric prompt operation
+        CancellationSignal cancellationSignal = new CancellationSignal();
+        cancellationSignal.setOnCancelListener(new CancellationSignal.OnCancelListener() {
+            @Override
+            public void onCancel() {
+                //handle cancel result
+                Log.i(TAG, "Canceled");
+            }
+        });
+        return cancellationSignal;
+    }
+
+
+    //지문인증 후 콜백
+    private BiometricPrompt.AuthenticationCallback getAuthenticationCallback() {
+        // Callback for biometric authentication result
+        return new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+                super.onAuthenticationHelp(helpCode, helpString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                Log.i(TAG, "onAuthenticationSucceeded");
+                super.onAuthenticationSucceeded(result);
+                Signature signature = result.getCryptoObject().getSignature();
+
+                Log.i(TAG, "Message: 인증 되었습니다.");
+
+                Toast.makeText(getApplicationContext(), "지문인증 완료", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        };
+    }
+
+
+
+
+    @Nullable
+    private KeyPair getKeyPair(String keyName) throws Exception {
+        KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+        keyStore.load(null);
+        if (keyStore.containsAlias(keyName)) {
+            // Get public key
+            PublicKey publicKey = keyStore.getCertificate(keyName).getPublicKey();
+            // Get private key
+            PrivateKey privateKey = (PrivateKey) keyStore.getKey(keyName, null);
+            // Return a key pair
+            return new KeyPair(publicKey, privateKey);
+        }
+        return null;
+    }
+
+    @Nullable
+    private Signature initSignature (String keyName) throws Exception {
+        KeyPair keyPair = getKeyPair(keyName);
+
+        if (keyPair != null) {
+            Signature signature = Signature.getInstance("SHA256withECDSA");
+            signature.initSign(keyPair.getPrivate());
+            return signature;
+        }
+        return null;
+    }
+}
